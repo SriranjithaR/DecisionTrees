a6ecb9906fd6c13e074a17bb70f34dee615c8436
SriranjithaR <sriranjitha.raghuraman@gmail.com>
2017-04-11 10:32:31 -0400

    Changed analysis 4

--- analysis_hw4.py
+++ analysis_hw4.py
@@ -430,18 +430,20 @@ class analysis:
 
         
 #            print avgzoldt
-            print avgzolrf
             print avgzolbag
+            print avgzolrf
 #            print avgzolsvm
             
 #            print stddevzoldt
-            print stddevzolrf
+
             print stddevzolbag
+            print stddevzolrf
 #            print stddevzolsvm
     
 #            print stderrzoldt
-            print stderrzolrf
+
             print stderrzolbag 
+            print stderrzolrf
 #            print stderrzolsvm   
     
             f = open(self.file,"a+");
@@ -496,31 +498,30 @@ class analysis:
         zoltempbag = [0 for xtemp in range(10)]
         zoltempsvm = [0 for xtemp in range(10)]
         
-        ratios = [0.25]
         w = 1000
         it = 10
         
-        avgzoldt  = [0 for xtemp in range(len(ratios))]
-        avgzolrf = [0 for xtemp in range(len(ratios))]
-        avgzolbag = [0 for xtemp in range(len(ratios))]
-        avgzolsvm = [0 for xtemp in range(len(ratios))]
+        avgzoldt  = [0 for xtemp in range(len(depths))]
+        avgzolrf = [0 for xtemp in range(len(depths))]
+        avgzolbag = [0 for xtemp in range(len(depths))]
+        avgzolsvm = [0 for xtemp in range(len(depths))]
         
-        stddevzoldt  = [0 for xtemp in range(len(ratios))]
-        stddevzolrf = [0 for xtemp in range(len(ratios))]
-        stddevzolbag = [0 for xtemp in range(len(ratios))]
-        stddevzolsvm = [0 for xtemp in range(len(ratios))]
+        stddevzoldt  = [0 for xtemp in range(len(depths))]
+        stddevzolrf = [0 for xtemp in range(len(depths))]
+        stddevzolbag = [0 for xtemp in range(len(depths))]
+        stddevzolsvm = [0 for xtemp in range(len(depths))]
         
-        stderrzoldt  = [0 for xtemp in range(len(ratios))]
-        stderrzolrf = [0 for xtemp in range(len(ratios))]
-        stderrzolbag = [0 for xtemp in range(len(ratios))]
-        stderrzolsvm = [0 for xtemp in range(len(ratios))]
+        stderrzoldt  = [0 for xtemp in range(len(depths))]
+        stderrzolrf = [0 for xtemp in range(len(depths))]
+        stderrzolbag = [0 for xtemp in range(len(depths))]
+        stderrzolsvm = [0 for xtemp in range(len(depths))]
         
                      
         testnew = []
         trainnew = []
         
 
-        for depth in depths:
+        for r in range(len(depths)):
                 
             for i in range(it):
                 trainnew = []
@@ -557,11 +558,11 @@ class analysis:
                 testfv, testfv0, testfv1 = featureVector(wordList[:w], x_test, y_test)
                
             
-                zoltempdt[i] = decisionTree(trainfv,testfv,depth)  
-                zoltemprf[i] = randomForest(trainfv,testfv,depth)   
-                zoltempbag[i] = bagging(trainfv,testfv,depth)      
+                zoltempdt[i] = decisionTree(trainfv,testfv,depths[r])  
+                zoltemprf[i] = randomForest(trainfv,testfv,depths[r])   
+                zoltempbag[i] = bagging(trainfv,testfv,depths[r])      
 #                    zoltempsvm[i] = svm(trainfv,testfv)   
-            r = 0    
+    
             avgzoldt[r]  = np.mean(zoltempdt)
             avgzolrf[r] = np.mean(zoltemprf)
             avgzolbag[r] = np.mean(zoltempbag)
@@ -579,53 +580,52 @@ class analysis:
         
 
         
-            print avgzoldt
-            print avgzolrf
-            print avgzolbag
+        print avgzoldt
+        print avgzolrf
+        print avgzolbag
 #            print avgzolsvm
-            
-            print stddevzoldt
-            print stddevzolrf
-            print stddevzolbag
+        
+        print stddevzoldt
+        print stddevzolrf
+        print stddevzolbag
 #            print stddevzolsvm
-    
-            print stderrzoldt
-            print stderrzolrf
-            print stderrzolbag 
+
+        print stderrzoldt
+        print stderrzolrf
+        print stderrzolbag 
 #            print stderrzolsvm   
-    
-            f = open(self.file,"a+");
-            f.write("\n Depth : " +  str(depth))
-            f.write("\n AVERAGE ZERO ONE LOSS")
-            f.write("\n 1. Decision Tree")
-            f.write(str(avgzoldt))
-            f.write("\n 2. Bagging")
-            f.write(str(avgzolbag))
-            f.write("\n 3. Random forest")
-            f.write(str(avgzolrf))
+
+        f = open(self.file,"a+");
+        f.write("\n AVERAGE ZERO ONE LOSS")
+        f.write("\n 1. Decision Tree")
+        f.write(str(avgzoldt))
+        f.write("\n 2. Bagging")
+        f.write(str(avgzolbag))
+        f.write("\n 3. Random forest")
+        f.write(str(avgzolrf))
 #            f.write("\n 4. SVM")
 #            f.write(str(avgzolsvm))
-            
-            f.write("\n STANDARD DEVIATION ZERO ONE LOSS")
-            f.write("\n 1. Decision Tree")
-            f.write(str(stddevzoldt))     
-            f.write("\n 2. Bagging")
-            f.write(str(stddevzolbag))        
-            f.write("\n 3. Random forest")
-            f.write(str(stddevzolrf))        
+        
+        f.write("\n STANDARD DEVIATION ZERO ONE LOSS")
+        f.write("\n 1. Decision Tree")
+        f.write(str(stddevzoldt))     
+        f.write("\n 2. Bagging")
+        f.write(str(stddevzolbag))        
+        f.write("\n 3. Random forest")
+        f.write(str(stddevzolrf))        
 #            f.write("\n 4. SVM")
 #            f.write(str(stddevzolsvm))
-            
-            f.write("\n STANDARD ERROR ZERO ONE LOSS")
-            f.write("\n 1. Decision Tree")
-            f.write(str(stderrzoldt))       
-            f.write("\n 2. Bagging")
-            f.write(str(stderrzolbag))        
-            f.write("\n 3. Random forest")
-            f.write(str(stderrzolrf))       
+        
+        f.write("\n STANDARD ERROR ZERO ONE LOSS")
+        f.write("\n 1. Decision Tree")
+        f.write(str(stderrzoldt))       
+        f.write("\n 2. Bagging")
+        f.write(str(stderrzolbag))        
+        f.write("\n 3. Random forest")
+        f.write(str(stderrzolrf))       
 #            f.write("\n 4. SVM")
 #            f.write(str(stderrzolsvm))
-            f.close();
+        f.close();
             
             
     def analysisDriver(self):
@@ -637,21 +637,22 @@ class analysis:
 #        f.close();
 #        self.changeRatios(ratios1, words1)
         
-        ratios2 = [0.25]
-        words2 = [200, 500, 1000, 1500]
-        f = open(self.file,"a+")
-        f.write("\n Analysis 2")
-        f.close();
-        self.changeWords(ratios2, words2)
-        
-        
-#        depths = [20]
+#        ratios2 = [0.25]
+#        words2 = [200, 500, 1000, 1500]
 #        f = open(self.file,"a+")
-#        f.write("\n Analysis 3")
+#        f.write("\n Analysis 2")
 #        f.close();
-#        self.changeDepth(depths)
+#        self.changeWords(ratios2, words2)
+        
+        
+        depths = [20]
+        f = open(self.file,"a+")
+        f.write("\n Analysis 3")
+        f.close();
+        self.changeDepth(depths)
         
 #        numTrees = [10,25,50,100]
+#        numTrees = [25]
 #        f = open(self.file,"a+")   
 #        f.write("\n Analysis 4")
 #        f.close();        
---
+++ plotting4.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 10:22:27 2017
+
+@author: Sriranjitha
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 10 23:38:41 2017
+
+@author: Sriranjitha
+"""
+
+import matplotlib.pyplot as plt
+
+# Make an array of x values
+x = [10,25,50,100]
+# Make an array of y values for each x value
+
+avgzolrf = [0.19600000000000004, 0.17050000000000004, 0.17850000000000002, 0.157]
+avgzolbag  = [0.23300000000000001, 0.1615, 0.18350000000000002, 0.1595]
+
+fig = plt.figure()
+
+
+#STANDARD ERROR ZERO ONE LOSS
+avgstderrrf = [0.01216141439142668, 0.0096033848199475972, 0.010584186317332094, 0.0092520268049763009]
+avgstderrbag  = [0.013605146085213493, 0.022978794572387821, 0.016659081607339584, 0.024662218067319091]
+
+plt.title('Analysis 4')
+plt.xlim(0,25)
+plt.xlabel('number of trees')
+plt.ylabel('zero-one loss')
+
+plt.errorbar(x = x, y = avgzolbag, yerr = avgstderrbag, capthick=2, label = 'BAG mean')
+plt.errorbar(x = x, y = avgzolrf, yerr = avgstderrrf,  capthick=2, label = 'RF mean')
+plt.legend(loc='upper right')
+# show the plot on the screen
+plt.show()
+
+
+fig.savefig('analysis2.png')
+
